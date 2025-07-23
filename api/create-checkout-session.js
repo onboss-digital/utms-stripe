a3d40a44abd608516edc3f5724221423714a168c
aldrichgg <alarchert@gmail.com>
2025-07-23 18:27:10 -0300

    cors

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -11,6 +11,10 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Max-Age', '86400'); 
     return res.status(200).end();
   }
 
